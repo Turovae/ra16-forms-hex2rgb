57a3a24d8f60f7210388a3cae7ba482d5f4788f8
TurovaE <ekaterina.turova@mail.ru>
2023-05-21 00:50:03 +0700

    create app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ resultColor, setResultColor ] = useState('rgb(255, 255, 255)');
+  const [ rgbCode, setRgbCode ] = useState('');
+
+  function changeInput(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+
+    console.log(event.target.value);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container' style={{backgroundColor: resultColor}}>
+        <input className='input' onChange={changeInput} />
+        <div className='result'>{rgbCode}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
